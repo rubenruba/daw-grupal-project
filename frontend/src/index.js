31f9b7dfa387eb774a39e653e10c5d9a8c17cdc6
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-01-19 10:23:00 +0100

    Fix root render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,8 +4,9 @@ import './styles/index.sass';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginPage } from './pages/Login/login';
 import { RegisterPage } from './pages/Register/register';
+import { LandginPage } from './pages/Landing/landing';
 
-const root = ReactDOM.createRoot(document.querySelector('body'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
     <Routes>
