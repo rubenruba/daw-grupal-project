a1c76ea02911a6f5ef240fba29a6c226d1c47a3b
Alba Silva <alumnos@clipdynamics.com>
2024-01-24 12:20:07 +0100

    Subida componentes footer y searchbar sin acabar

---
+++ frontend/src/components/Footer/footer.jsx
@@ -0,0 +1,13 @@
+import './footer.sass';
+
+export const FooterComponent = () => {
+    // JS
+
+
+    // HTML
+    return (
+        <footer>
+            <input type="text" name="search" id="" />
+        </footer>
+    )
+}
---
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,14 @@
+import './SearchBar.sass';
+
+export const SearchBarComponent = () => {
+    // JS
+
+
+    // HTML
+    return (
+        <div id="searchBar">
+            <input type="text" name="search" id="SearchBar" placeholder='Search...'/>
+            <img src="../../assets/Search.png" alt="search button" />
+        </div>
+    )
+}
