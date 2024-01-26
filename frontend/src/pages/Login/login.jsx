694db0489234eb3a539d0f80288cfdee2e12b843
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-01-26 12:48:09 +0100

    Start connectin login to database

--- frontend/src/pages/Login/login.jsx
+++ frontend/src/pages/Login/login.jsx
@@ -2,11 +2,12 @@ import "./login.sass";
 
 export const LoginPage = () => {
   // JS
+  const url = 'http://localhost/testFinalProjects/login.php';
 
   // HTML
   return (
     <div className="d-flex flex-column justify-content-center align-items-center p-4 login-container">
-      <form action="#" className="p-3 sm-mx-300">
+      <form action={url} method="post" className="p-3 sm-mx-300">
         <h2 className="fw-bold">SIGN IN</h2>
         <label>Email</label>
         <input type="text" className="border-0 " />
@@ -18,7 +19,7 @@ export const LoginPage = () => {
           <a href="#">Olvidaste tu contraseña?</a>
         </label>
         <label href="#" className="mb-3">
-          <a href="#">No tienes cuenta? Regístrate ahora</a>
+          <a href="/register">No tienes cuenta? Regístrate ahora</a>
         </label>
 
         <button className="btn btn-primary btn-md w-50">
