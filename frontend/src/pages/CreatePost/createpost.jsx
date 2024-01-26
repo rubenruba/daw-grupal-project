a0a06d10a3180d56ccbb6cc2437e13efdaa252f7
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-01-26 09:31:14 +0100

    Fix styles

--- frontend/src/pages/CreatePost/createpost.jsx
+++ frontend/src/pages/CreatePost/createpost.jsx
@@ -1,3 +1,4 @@
+import { FooterComponent } from '../../components/Footer/footer';
 import './createpost.sass';
 
 export const CreatePostPage = () => {
@@ -7,35 +8,38 @@ export const CreatePostPage = () => {
     // HTML
     return (
         <>
+        {/* TO DO - Make header a component */}
             <header>
                 <img src='/logo.png' alt="" id='logo' />
                 <input type="text" placeholder=' Search... ' className='form-control' id='cabe' />
                 <img src="/circle-user.png" alt="" id='user' />
             </header>
-            <body>
+            <section className='create-post-container pt-5 pb-5'>
                 <form action="">
-                    <h3>Make a post</h3>
+                    <h3 className='text-center fw-bold'>MAKE A POST</h3>
                     <div id='contenido'>
                         <p>Título</p>
-                        <input type="text" className='form-control' id='i1' />
-                        <p id='p2'>Texto</p>
-                        <input type="text" className='form-control' id='i2' />
+                        <input type="text" className='form-control'/>
+                        <p className='mt-4'>Texto</p>
+                        <textarea type="text" className='form-control' rows="15" />
                     </div>
                     <div id='enlace'>
                         <div>
-                            <p id='padj'>Adjuntar archivos</p>
-                            <img src="/adjuntar.png" alt="" id='adjunta' />
+                            <p className='mt-5'>Adjuntar archivos</p>
+                            {/* TO DO - Add an input to insert files */}
+                            <img src="/adjuntar.png" alt="" />
                         </div>
-                        <div id='enlace2'>
-                            <button id='btn1'>Etiquetas
-                                <img src="/mas.png" alt="" id='mas' />
+                        <div className='d-flex flex-column'>
+                            <button id='btn1' className='p-2'>
+                                Etiquetas
+                                <img src="/mas.png" alt="" id='mas' className='ms-4'/>
                             </button>
-                            <button id='btn2'>Publicar</button>
+                            <button id='btn2' className='p-2 mt-5'>Publicar</button>
                         </div>
                     </div>
-
                 </form>
-            </body>
+            </section>
+            <FooterComponent></FooterComponent>
         </>
     )
 }
