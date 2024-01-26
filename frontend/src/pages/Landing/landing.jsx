685081d1d3bae5e6a0eb5ce368c72aec89915b4f
KevinB00 <kevbueeva@alu.edu.gva.es>
2024-01-26 12:41:04 +0100

    Diseño de Landing page, falta ampliar, y revisar media querys

--- frontend/src/pages/Landing/landing.jsx
+++ frontend/src/pages/Landing/landing.jsx
@@ -7,18 +7,39 @@ export const LandginPage = () => {
 
   // HTML
   return (
-      <div className="d-flex flex-column justify-content-center align-items-center p-4 landing">
-        <div>
-          <SearchBarComponent></SearchBarComponent>
-        </div>
-        <div className="d-flex justify-content-center align-items-center p-5 container">
-            <div className="d-flex flex-column justify-content-center align-items-center box1">
-              <p>Find any type of files related to your studies and more</p>
-              <button className="btn btn-primary btnStartSearch">Start searching</button>
-            </div>
+    <>
+      <div className="d-flex flex-column align-items-center landing">
+        <header>
+          <img src="/logo.png" alt="" id="logo" />
+          <input
+            type="text"
+            placeholder=" Search... "
+            className="form-control"
+            id="cabe"
+          />
+          <img src="/circle-user.png" alt="" id="user" />
+        </header>
+        <div className="d-flex flex-column justify-content-center align-items-center p-5 container">
+          <div className="d-flex flex-column justify-content-center align-items-center p-3 mt-5  box1">
+            <p>Find any type of files related to your studies and more</p>
+            <button className="btn btn-primary p-2 btnStartSearch">
+              Start searching
+            </button>
+          </div>
+          <div className="d-flex flex-column justify-content-center align-items-center p-3 mt-5 box2">
+              <p>Share files with other students around the world</p>
+              <button className="btn btn-primary p-2 btnJoinNow">Join Now</button>
+          </div>
+          <div className="d-flex flex-column justify-content-center align-items-center p-3 mt-5 box3">
+              <p>Communicate with your friends to solve study problems through a chat and where you can create groups.</p><br />
+              <img src="/guardar.png" alt="guardar_post" />
+              <p>Find the most outstanding answer to get the best option and understand the problem you had, you can hit save to raise the score to that answer and make it more popular and help more people who have the same problem.</p>
+          </div>
+
         </div>
 
         <FooterComponent></FooterComponent>
-      </div> 
+      </div>
+    </>
   );
 };
