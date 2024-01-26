6998ded2a196d43fd0092aed939d62220b2db068
Marcos <marcosugenaochando@gmail.com>
2024-01-26 12:28:13 +0100

    Header Casi hecho falta revisar,footer ajustado y funcionalidad de adjunatar archivos añadida

--- frontend/src/pages/CreatePost/createpost.jsx
+++ frontend/src/pages/CreatePost/createpost.jsx
@@ -3,15 +3,17 @@ import './createpost.sass';
 
 export const CreatePostPage = () => {
     // JS
-
-
+        const test = () => document.getElementById('fileInput').click();
     // HTML
     return (
         <>
-        {/* TO DO - Make header a component */}
+            {/* TO DO - Make header a component */}
             <header>
                 <img src='/logo.png' alt="" id='logo' />
-                <input type="text" placeholder=' Search... ' className='form-control' id='cabe' />
+                <div className='d-flex justify-content-between' id='search-container'>
+                <input type="text" placeholder=' Search... ' className='form-control me-4' id='cabe'/>
+                <img src="/lupa.png" alt="" />
+                </div>
                 <img src="/circle-user.png" alt="" id='user' />
             </header>
             <section className='create-post-container pt-5 pb-5'>
@@ -19,7 +21,7 @@ export const CreatePostPage = () => {
                     <h3 className='text-center fw-bold'>MAKE A POST</h3>
                     <div id='contenido'>
                         <p>Título</p>
-                        <input type="text" className='form-control'/>
+                        <input type="text" className='form-control' />
                         <p className='mt-4'>Texto</p>
                         <textarea type="text" className='form-control' rows="15" />
                     </div>
@@ -27,12 +29,13 @@ export const CreatePostPage = () => {
                         <div>
                             <p className='mt-5'>Adjuntar archivos</p>
                             {/* TO DO - Add an input to insert files */}
-                            <img src="/adjuntar.png" alt="" />
+                            <input type="file" id="fileInput" style={{ 'display': 'none' }}></input>
+                            <img src="/adjuntar.png" alt="" className='' id='adjunta' onClick={test}/>
                         </div>
                         <div className='d-flex flex-column'>
                             <button id='btn1' className='p-2'>
                                 Etiquetas
-                                <img src="/mas.png" alt="" id='mas' className='ms-4'/>
+                                <img src="/mas.png" alt="" id='mas' className='ms-4' />
                             </button>
                             <button id='btn2' className='p-2 mt-5'>Publicar</button>
                         </div>
