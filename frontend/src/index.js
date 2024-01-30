2806784a458ad4e80868b23dc800d3d84d5fe004
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-01-30 13:39:04 +0100

    Init with admin view pending revision

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import { LoginPage } from './pages/Login/login';
 import { RegisterPage } from './pages/Register/register';
 import { LandginPage } from './pages/Landing/landing';
 import { CreatePostPage } from './pages/CreatePost/createpost';
+import { AdminUsersPage } from './pages/AdminUsers/adminUsers';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,6 +16,7 @@ root.render(
       <Route path="login" element={ <LoginPage/> } />
       <Route path="register" element={ <RegisterPage/> } />
       <Route path="create-post" element={ <CreatePostPage/> } />
+      <Route path="admin-users" element={ <AdminUsersPage/> } />
   </Routes>
 </BrowserRouter>
 );
