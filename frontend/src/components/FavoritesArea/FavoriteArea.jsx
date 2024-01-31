c853704b76ffbb900c6185656b4120dd780ed027
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-01-31 10:35:59 +0100

    start with saving favourites

--- frontend/src/components/FavoritesArea/FavoriteArea.jsx
+++ frontend/src/components/FavoritesArea/FavoriteArea.jsx
@@ -1,16 +1,35 @@
 import './FavoriteArea.sass';
 import React, { useState } from 'react';
 
-export const FavoriteArea = () => {
+export const FavoriteArea = (post) => {
     // JS
+    const url = 'http://localhost/testFinalProjects/insertData/addFavourite.php';
+    const [starPath, setStarPath] = useState('/svg/star-outlined.svg');
 
+    const changePath = () => {
+        if(starPath === '/svg/star-outlined.svg'){
+            setStarPath('/svg/star-solid.svg');
+        } else {
+            setStarPath('/svg/star-outlined.svg');
+            console.log('CAMBIA')
+        }
+    }
+
+    const addFavourite = () => {
+        fetch(`${url}?postId=${post.PostId}`, {
+            method: 'POST',
+            // body
+        })
+    }
 
     // HTML
     return (
         <div className="container-f">
             <div className="simbolos">
                 <img src="/preguntas-mas-frecuentes.png" alt="" />
-                <img src="/star.png" alt="" />
+                <button className='star' onClick={changePath}>
+                    <img src={starPath} alt="Star"/>
+                </button>
             </div>
             <div className="favorite-text">
                 <p>LOREM IPSUM DOLOR SIT AMET, CONSECTETUR ADIPISCING ELIT. INTEGER SED FEUGIAT NUNC, ET VARIUS RISUS?</p>
