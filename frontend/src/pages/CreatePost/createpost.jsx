7be286f5be9ae5f3f90a185458248b0a7895764b
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-02-01 23:47:32 +0100

    Add menu and buttonsNav components and fix some styles

--- frontend/src/pages/CreatePost/createpost.jsx
+++ frontend/src/pages/CreatePost/createpost.jsx
@@ -10,7 +10,6 @@ export const CreatePostPage = () => {
     // HTML
     return (
         <>
-            {/* TO DO - Make header a component */}
             <HeaderComponent></HeaderComponent>
             <section className='create-post-container pt-5 pb-5'>
                 <form action={url} method='post' enctype="multipart/form-data">
@@ -21,20 +20,29 @@ export const CreatePostPage = () => {
                         <p className='mt-4'>Texto</p>
                         <textarea type="text" className='form-control' rows="15" name='postText'/>
                     </div>
-                    <div id='enlace' className='d-flex flex-lg-row align-items-lg-center justify-content-evenly'>
+                    <div className='labels-container'>
+                        <p className='mb-3'>Etiquetas</p>
+                        <div className='d-flex flex-wrap labels'>
+                            <select>
+                                <option value=''>None</option>
+                                <option value=''>None</option>
+                            </select>
+                            <select>
+                                <option value=''>None</option>
+                            </select>
+                            <select>
+                                <option value=''>None</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div id='enlace' className='d-flex flex-lg-row align-items-lg-center justify-content-between flex-wrap'>
                         <div className='mb-4'>
-                            <p className='mt-5' id='parr'>Adjuntar archivos</p>
+                            <p className='mt-5'>Adjuntar archivos</p>
                             {/* TO DO - Add an input to insert files */}
                             <input type="file" id="fileInput" name='files[]' multiple/>
                             <img src="/adjuntar.png" id='adjunta' alt="" />
                         </div>
-                        <div className='d-flex flex-column'>
-                            <button id='btn1' className='p-2'>
-                                Etiquetas
-                                <img src="/mas.png" alt="" id='mas' className='ms-4' />
-                            </button>
-                            <button id='btn2' className='p-2 mt-5'>Publicar</button>
-                        </div>
+                        <button className='p-2 mt-5'>Publicar</button>
                     </div>
                 </form>
             </section>
