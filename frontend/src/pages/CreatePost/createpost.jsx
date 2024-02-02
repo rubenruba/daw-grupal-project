ef5245588c3821537263c6d98d24f77514ec5609
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-02-02 10:07:21 +0100

    Add labels select options

--- frontend/src/pages/CreatePost/createpost.jsx
+++ frontend/src/pages/CreatePost/createpost.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { FooterComponent } from '../../components/Footer/footer';
 import { HeaderComponent } from '../../components/Header/header';
 import './createpost.sass';
@@ -5,14 +6,64 @@ import './createpost.sass';
 export const CreatePostPage = () => {
     // JS
     // const url = 'url to aws';
-    const url = 'http://localhost/testFinalProjects/insertData/insertPost.php';
-  
+    const urlPost = 'http://localhost/testFinalProjects/insertData/insertPost.php';
+    const urlLabel = 'http://localhost/testFinalProjects/retrieveData/getAllLabels.php';
+    const [labels, setLabels] = useState([]);
+    const [labelsCopy, setLabelsCopy] = useState([]);
+    const [selected, setSelected] = useState([]);
+    
+    
+    useEffect(() => {
+        fetch(urlLabel)
+        .then((response) => {
+            if(!response.ok){
+                throw new Error('Error al obtener etiquetas');
+            } else {
+                return response.json();
+            }
+        })
+        .then((data) => {
+            setLabels(data);
+            setLabelsCopy(data);
+        })
+        .catch((error) => {
+            console.log('ERROR', error);
+        })
+    }, []);
+
+    // const checkSelected = () => {
+    //     const arr = Array.from(labels.map(lab => {return lab.LabelId.toString()}))
+
+    //     // selected.forEach(labelId => {
+    //     //     if(arr.includes(labelId)){
+    //     //         console.log('INCLUYE')
+    //     //         setLabelsCopy(labelsCopy.filter((lab) => lab.LabelId.toString() !== labelId))
+    //     //     }
+    //     // });
+
+    //     const newArr = [];
+    //     labels.forEach((lab) => {
+    //         if(!selected.includes(lab.LabelId.toString())){
+    //             newArr.push(lab);
+    //             console.log('NO INCLUYE')
+    //         }
+    //     })
+
+    //     setLabelsCopy(newArr);
+    // }
+
+    // const selectLabel = (labelId, pos) => {
+    //     checkSelected();
+    //     setSelected([selected[pos] = labelId, ...selected])
+    //     console.log(selected)
+    // }
+
     // HTML
     return (
         <>
             <HeaderComponent></HeaderComponent>
             <section className='create-post-container pt-5 pb-5'>
-                <form action={url} method='post' enctype="multipart/form-data">
+                <form action={urlPost} method='post' enctype="multipart/form-data">
                     <h3 className='text-center fw-bold'>MAKE A POST</h3>
                     <div id='contenido'>
                         <p>Título</p>
@@ -23,15 +74,24 @@ export const CreatePostPage = () => {
                     <div className='labels-container'>
                         <p className='mb-3'>Etiquetas</p>
                         <div className='d-flex flex-wrap labels'>
-                            <select>
+                            {/* <select onChange={(e) => selectLabel(e.target.value, 0)}> */}
+                            <select name='label1'>
                                 <option value=''>None</option>
-                                <option value=''>None</option>
+                                {labelsCopy.map(label => {
+                                    return <option value={label.LabelId}>{label.Name}</option>
+                                })}
                             </select>
-                            <select>
+                            <select name='label2'>
                                 <option value=''>None</option>
+                                {labelsCopy.map(label => {
+                                    return <option value={label.LabelId}>{label.Name}</option>
+                                })}
                             </select>
-                            <select>
+                            <select name='label3'>
                                 <option value=''>None</option>
+                                {labelsCopy.map(label => {
+                                    return <option value={label.LabelId}>{label.Name}</option>
+                                })}
                             </select>
                         </div>
                     </div>
