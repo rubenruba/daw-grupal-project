b9a8c828d2a698722e16ec07602400f444bda444
Marcos <marcosugenaochando@gmail.com>
2024-02-12 10:12:31 +0100

    API con palabras malsonantes implemetada con condicion falta cancelar el formulario en caso malo y enviar formulario en caso bueno

--- frontend/src/pages/CreatePost/createpost.jsx
+++ frontend/src/pages/CreatePost/createpost.jsx
@@ -5,10 +5,11 @@ import './createpost.sass';
 
 export const CreatePostPage = () => {
     const [apiResponse, setApiResponse] = useState(null);
-   
-     const corta = (e) => {
+
+    const corta = (e) => {
         e.preventDefault();
-     }
+
+    }
     // Función para manejar el clic en el botón Publicar
     const handlePublish = async () => {
         // Obtener el texto del textarea
@@ -21,21 +22,49 @@ export const CreatePostPage = () => {
                 'X-RapidAPI-Host': 'community-purgomalum.p.rapidapi.com'
             }
         };
-
         try {
-            const api=document.getElementById("apitext").textContent;
+            let palabrasInapropiadas = [
+                "cabrón", "joder", "puta", "pendejo", "coño", "mierda", "maricón", "zorra", "idiota", "malparido",
+                "perra", "bastardo", "hijoputa", "mamón", "gilipollas", "chinga", "coger", "desgraciado", "pelotudo",
+                "cabronazo", "culero", "chupapijas", "cachonda", "chupar", "pito", "verga", "puñeta", "chingar", "pajero",
+                "mierdita", "pendeja", "cabrón", "putazo", "mamahuevo", "mamaverga", "huevón", "pendejada", "pirobo",
+                "gilipuertas", "cagón", "estúpido", "imbécil", "cabestro", "cabrón", "pinche", "culebra", "tonto",
+                "tarado", "baboso", "putañero", "carajo", "pichula", "pajúa", "cachar", "culear", "reconchetumare",
+                "culeado", "putamadre", "papaya", "putero", "jodido", "pichabrava", "mierdecilla", "pelmazo",
+                "malnacido", "cachucha", "mariposón", "mariposón", "puñal", "carajo", "chingón", "reputo",
+                "joputa", "putero", "pichicorta", "reputo", "puticlub", "capullo", "trolo", "putarra", "malparido",
+                "fornicar", "chichi", "pipí", "pimpirrín", "chuminada", "mierdecilla", "culero", "mecos", "huevón",
+                "pajillero", "joputa", "mariposón", "marica", "pajillero", "meón", "mojigato", "papafrita", "papelón",
+                "pajarraco", "mojón", "malparido", "merdellón", "mariconazo", "maricón", "manco", "jilipollas",
+                "pendejón", "papanatas", "pajote", "nabazo", "mariposilla", "naco", "mierdón", "jilipollas",
+                "pendejazo", "pajuelo", "mona", "papapollas", "papanatas", "mojón", "pinchurriento", "pantallero",
+                "papanatas", "jilipollas", "papafrita", "mierdón", "mariquita", "panoli", "mamón", "mamón", "putañero",
+                "putero", "pene", "mierda", "puticlub", "pito", "pelotas", "pene", "perro", "pelado", "papanatas",
+                "panocha", "mariconazo", "marica", "mamar", "jilipollas", "jilipollas", "pendejazo", "pendejo",
+                "pendejada", "pelotazo", "pajón", "pajoso", "pajillero", "pajero", "pajarraco", "pajillero", "paquete",
+                "pajarraco", "parásito", "patán", "patético", "payaso", "pechofrío", "pedo", "pendejo", "pene", "picarón",
+                "pijo", "pinche", "pipí", "pirobo", "pito", "putero", "puta", "putear", "putero", "pichicorta",
+                "putamierda", "putear", "putañero", "puta", "putañero", "putarra", "putarraca", "puticlub", "putidifuso",
+                "putiengue", "putiinferno", "puto", "putón", "putrefacto", "putrefacto", "putrefacto", "puturrú",
+                "puturrú", "puturrú", "puñal", "puñalada", "puñalazo", "puñalón", "puñetazo", "puñetero", "puñetero",
+                "puñetero", "puño", "putazo", "putazo", "putazo", "putear", "putear", "putear", "putero", "puti",
+                "puto", "putón", "putón", "putón", "putón", "putrefacto", "putrefacto", "putrefacto", "puturrú"];
+            const api = document.getElementById("apitext").value;
             const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
-            setApiResponse(result);
-            
+            const result = await response.json();
+            console.log(result.result);
+            console.log(api)
+            if (result.result != api || palabrasInapropiadas.includes(api)) {
+                let form = document.getElementById("formularioapi");
+                console.log("cortando 2")
+                form.addEventListener("submit", function (e) {
+                    corta(e);
+                })
+            }
         } catch (error) {
             console.error(error);
         }
     };
-
-    
-
     // HTML
     return (
         <>
@@ -53,7 +82,7 @@ export const CreatePostPage = () => {
                         <div className='mb-4'>
                             <p className='mt-5' id='parr'>Adjuntar archivos</p>
                             <input type="file" id="fileInput" style={{ 'display': 'none' }}></input>
-                            <img src="/adjuntar.png" alt="" className='' id='adjunta'/>
+                            <img src="/adjuntar.png" alt="" className='' id='adjunta' />
                         </div>
                         <div className='d-flex flex-column'>
                             <button id='btn1' className='p-2'>
