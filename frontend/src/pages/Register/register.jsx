3419d1b97928b5c8c50ca86f1f1d5ee827cce0b6
Rubén Martínez <rubenn.mmartinezz@gmail.com>
2024-02-14 22:00:09 +0100

    Add required in register inputs

--- frontend/src/pages/Register/register.jsx
+++ frontend/src/pages/Register/register.jsx
@@ -10,16 +10,16 @@ export const RegisterPage = () => {
     <div className="register-container d-flex align-items-center justify-content-center">
       <form action={url} method='post' className="p-3 sm-mx-300">
         <h2 className="fw-bold">REGISTER NOW</h2>
-        <label className="">Name</label>
-        <input className="form-control" type="text" name="name"/>
+        <label>Name</label>
+        <input className="form-control" type="text" name="name" required/>
         <label>SurName</label>
-        <input className="form-control" type="text" name="surname"/>
+        <input className="form-control" type="text" name="surname" required/>
         <label>Password</label>
-        <input className="form-control" type="password" name="password"/>
+        <input className="form-control" type="password" name="password" required/>
         <label>UserName</label>
-        <input className="form-control" type="text" name="username"/>
+        <input className="form-control" type="text" name="username" required/>
         <label>Email</label>
-        <input className="form-control" type="text" name="email"/>
+        <input className="form-control" type="text" name="email" required/>
         <a href="/login" className="mt-4">¿Ya tienes una cuenta?</a>
         <button className="btn btn-primary btn-md">Register</button>
       </form>
